6580b25d17130c3a39f27c3e7223ef72941c9058
daiysh <569527318@qq.com>
2020-04-12 22:20:28 +0800

    update

--- config/database.php
+++ config/database.php
@@ -21,7 +21,7 @@ return [
             // 数据库类型
             'type'              => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'          => env('database.hostname', '127.0.0.1'),
+            'hostname'          => env('database.hostname', 'www.innerbboy.cn'),
             // 数据库名
             'database'          => env('database.database', 'database'),
             // 用户名
